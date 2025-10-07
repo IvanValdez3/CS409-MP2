3ebee10e033f4e543d015844743d0c552b070a2e
unknown <LocalAdmin@ENGRIT-LOAN-F02.ad.uillinois.edu>
2025-10-06 22:35:46 -0500

    Added setUserInput function. Next will work on list view and will add a Name to the site for user-experience'

--- mp2/src/App.tsx
+++ mp2/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css';
+import { useState } from 'react';
 import SearchBar from './Components/SearchBar/SearchBar';
 
 function App() {
+
+  const [userInput, setUserInput] = useState('');
+
   return (
     <div className="App">
       <header className="Search-header">
-        <SearchBar />
+        <SearchBar userInput={userInput} setUserInput={setUserInput} />
       </header>
     </div>
   );
 }
+
 
 export default App;
--- mp2/src/Components/SearchBar/SearchBar.js
+++ mp2/src/Components/SearchBar/SearchBar.js
@@ -1,10 +1,16 @@
 import './SearchBar.css';
 
 
-function SearchBar() {
+function SearchBar({userInput, setUserInput}) {
+
+    //Most important is handling text input
+    const input = (event) => {
+        setUserInput(event.target.value);
+    };
+
   return (
     <div className="SearchBar">
-      <input className="Search-input"type="text" placeholder="Search..." />
+      <input className="Search-input" type="text" placeholder="Search..." value={userInput} onChange={input} />
       <button className="Search-button" type="submit">Search</button>
     </div>
   );
